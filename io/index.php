e8d7f8fcde533bc182f4f3b6bfbdfcc47a055f36
UenoRuuku <18302010013@fudan.edu.cn>
2020-10-16 23:17:45 +0800

    I forget to save

--- io/index.php
+++ io/index.php
@@ -1,21 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-function connectMysql()
-{
-    $dbms = 'mysql';     //数据库类型
-    $host = 'localhost'; //数据库主机名
-    $dbName = 'io';    //使用的数据库
-    $user = 'root';      //数据库连接用户名
-    $pass = '';          //对应的密码
-    try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbName", $user, $pass);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-}
+include "connect.php";
 
 function login()
 {
--- io/unlock.php
+++ io/unlock.php
@@ -1,20 +1,5 @@
 <?php
-function connectMysql()
-{
-    $dbms = 'mysql';     //数据库类型
-    $host = 'localhost'; //数据库主机名
-    $dbName = 'io';    //使用的数据库
-    $user = 'root';      //数据库连接用户名
-    $pass = '';          //对应的密码
-    try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbName", $user, $pass);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-}
-$conn = connectMysql();
+include "connect.php";
 
 $stmt3 = $conn->prepare("SELECT * FROM groups WHERE name = '".$_POST["name"]."'");
 $stmt3->execute();
