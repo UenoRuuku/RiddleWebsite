c89d9f2dfee2d3f8d81513f7bcc3f8d419578a91
UenoRuuku <18302010013@fudan.edu.cn>
2020-10-16 22:16:37 +0800

    fix more gus

--- io/CtrlA.php
+++ io/CtrlA.php
@@ -9,7 +9,7 @@
     <p style="display:none">答案是：南区食堂1楼饭卡机器旁边</p>
     <?php 
     if(isset($_GET['a']) && $_GET['a'] == "30"){
-        if(isset($_GET['b']) && $_GET['b'] == "12"){
+        if(isset($_GET['b']) && $_GET['b'] == "9"){
             echo '<script>alert("应该不会有人以为是南区食堂1楼饭卡机旁边吧，答案在南区学生超市哦");</script>';
         }
     }
