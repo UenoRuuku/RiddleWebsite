49aa59307423aa42425b9f25e4033585b2f825b7
UenoRuuku <18302010013@fudan.edu.cn>
2020-10-18 01:42:51 +0800

    change forbidden time

--- io/goToForbidden.php
+++ io/goToForbidden.php
@@ -1,5 +1,5 @@
 <?php
     date_default_timezone_set("Asia/Shanghai");
-    if(date('G')<7 && date('G')>23){
+    if(date('G')<6 || date('G')>23){
         echo "<script>location.href='forbidden.php'</script>";
     }
